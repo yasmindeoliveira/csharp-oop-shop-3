9a187320f474ed2fb1237988276793f009deddeb
yasmindeoliveira <yaz.oliveira01@gmail.com>
2022-12-06 15:18:28 +0100

    Inserito il Programma

--- CSharpShop3/Program.cs
+++ CSharpShop3/Program.cs
@@ -1,2 +1,26 @@
 ﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+using CSharpShop3;
+
+Acqua Bottiglia1 = new Acqua("Sant'Anna", "Acqua Minerale", 0.20f, 22, 1.5f, 6.9f, "Sant'Anna");
+Bottiglia1.Stampa();
+
+Bottiglia1.BeviAcqua(1);
+Bottiglia1.Stampa();
+
+Bottiglia1.RiempiBottiglia(0.7f);
+Bottiglia1.Stampa();
+
+Bottiglia1.SvuotaBottiglia();
+Bottiglia1.Stampa();
+
+FruttaAPezzi Ananas = new FruttaAPezzi("Ananas", "Bromeliaceae", "Frutta", 1.5f, 10, true);
+Ananas.Stampa();
+
+Ananas.MangiaFrutta(4);
+Ananas.Stampa();
+
+Ananas.RiempiSacchetto(1);
+Ananas.Stampa();
+
+Prodotto lavatriceBosch = new Prodotto("LavatriceBosch", "Per bucato", 100, 22);
+lavatriceBosch.Stampa();
