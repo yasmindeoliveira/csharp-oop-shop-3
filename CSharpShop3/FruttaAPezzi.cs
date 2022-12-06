69f71f3ccdfc936995be15a640005d1c4d2ff8c8
yasmindeoliveira <yaz.oliveira01@gmail.com>
2022-12-06 15:12:48 +0100

    Inserita classe FruttaAPezzi

---
+++ CSharpShop3/FruttaAPezzi.cs
@@ -0,0 +1,98 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CSharpShop3
+{
+    public class FruttaAPezzi : Prodotto
+    {
+        // CARATTERISTICHE
+        private string tipoFrutto;
+        private uint quantita;
+
+        // STATO
+        private bool secca;
+
+        // COSTRUTTORE
+        public FruttaAPezzi(string nome, string tipoFrutto, string descrizione, float prezzo, int iva, bool secca = false) : base(nome, descrizione, prezzo, iva)
+        {
+            this.tipoFrutto = tipoFrutto;
+            this.quantita = 5;
+            this.secca = secca;
+        }
+
+        // GETTERS
+
+        public string GetTipoFrutto()
+        {
+            return this.tipoFrutto;
+        }
+        public uint GetQuantita()
+        {
+            return this.quantita;
+        }
+        public bool GetSecca()
+        {
+            return this.secca;
+        }
+
+        // SETTERS
+
+        public void SetTipoFrutto(string tipoFrutto)
+        {
+            this.tipoFrutto = tipoFrutto;
+        }
+        public void SetQuantita(uint quantita)
+        {
+            this.quantita = quantita;
+        }
+        public void SetSecca(bool secca)
+        {
+            this.secca = secca;
+        }
+
+        // METODO
+
+        public uint MangiaFrutta(uint pezziDaMangiare)
+        {
+            if (pezziDaMangiare <= this.quantita)
+            {
+                if (this.quantita > pezziDaMangiare)
+                {
+                    this.quantita = this.quantita - pezziDaMangiare;
+                }
+                else
+                {
+                    this.quantita = 0;
+                    Console.WriteLine("Hai finito la frutta.");
+                }
+            }
+            else
+            {
+                Console.WriteLine("Non puoi mangiare frutta che non c'è.");
+            }
+
+            return this.quantita;
+        }
+
+        public void RiempiSacchetto(uint fruttaDaInserire)
+        {
+            this.quantita += fruttaDaInserire;
+        }
+
+        public override void Stampa()
+        {
+            base.Stampa();
+            Console.WriteLine();
+            Console.WriteLine("Dati frutta:");
+            Console.WriteLine();
+            Console.WriteLine("Tipo Frutta: " + GetTipoFrutto());
+            Console.WriteLine("Quantità: " + GetQuantita());
+            Console.WriteLine("Secca? " + GetSecca());
+            Console.WriteLine();
+
+        }
+    }
+}
